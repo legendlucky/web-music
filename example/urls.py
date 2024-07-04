db81d7118f77775e8d5dc2d06241e80aa0905325
legendlucky <legendluckyraj@gmail.com>
2024-07-04 04:14:10 +0000

    adding real music functionality

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
-# example/urls.py
 from django.urls import path
-
-from example.views import index
-
+from . import views
 
 urlpatterns = [
-    path('', index),
+    path('record/', views.home, name='home'),
+    path('', views.record, name='record'),
+    path('save_speech_text/', views.save_speech_text, name='save_speech_text'),
+    path('song/', views.display_text, name='song'),
 ]
+
+handler404 = 'home.views.custom_404'
